20c1042de55a6a4746de2eaabed39aad2ff579fe
Fabian Mink THM <fabian.mink@iem.thm.de>
2022-06-23 12:09:23 +0200

    new version with two seperate coils

--- fem/aircoil_FEMM/aircoil.lua
+++ fem/aircoil_FEMM/aircoil.lua
@@ -1,14 +1,19 @@
-Icoil = 10 
-Ncoil = 500
+Icoil1 = 20 
+Ncoil1 = 100
+
+Icoil2 = 0 
+Ncoil2 = 100
 
 ky = 0
 kr = 6
 
-rbound = 75
+--rbound = 75
+rbound = 150
 
 rx = 8
 d = 4
-h = 20
+h = 10
+h_sh = 1
 
 am = 1 --Automesh on
 ms = 10 --Mesh size (optional, only if automesh off)
@@ -16,7 +21,8 @@ ms = 10 --Mesh size (optional, only if automesh off)
 file = openfile("output.csv", "w")
 
 istep = 100
-for ky=-12,2,0.5 do 
+for ky=-30,8,0.5 do 
+--for ky=-12,0,2 do 
 
 
 newdocument(0)
@@ -29,18 +35,25 @@ mi_addnode(0,ky-kr)
 mi_addarc(0,(ky-kr),0,(ky+kr),180,1)
 
 	
---Spulen konstruieren
-mi_addnode(rx,-h/2)
-mi_addnode(rx,h/2)
-mi_addnode(rx+d,-h/2)
-mi_addnode(rx+d,h/2)
+--Spule 1 konstruieren
+mi_addnode(rx,-h-h_sh)
+mi_addnode(rx,-h_sh)
+mi_addnode(rx+d,-h-h_sh)
+mi_addnode(rx+d,-h_sh)
+mi_addsegment(rx,-h-h_sh,rx,-h_sh)
+mi_addsegment(rx,-h_sh,rx+d,-h_sh)
+mi_addsegment(rx+d,-h_sh,rx+d,-h-h_sh)
+mi_addsegment(rx+d,-h-h_sh,rx,-h-h_sh)
 
-
-mi_addsegment(rx,-h/2,rx,h/2)
-mi_addsegment(rx,h/2,rx+d,h/2)
-mi_addsegment(rx+d,h/2,rx+d,-h/2)
-mi_addsegment(rx+d,-h/2,rx,-h/2)
-
+--Spule 2 konstruieren
+mi_addnode(rx,h+h_sh)
+mi_addnode(rx,h_sh)
+mi_addnode(rx+d,h+h_sh)
+mi_addnode(rx+d,h_sh)
+mi_addsegment(rx,h+h_sh,rx,h_sh)
+mi_addsegment(rx,h_sh,rx+d,h_sh)
+mi_addsegment(rx+d,h_sh,rx+d,h+h_sh)
+mi_addsegment(rx+d,h+h_sh,rx,h+h_sh)
 
 --Punkte Begrenzung zu berechnender Bereich
 mi_addnode(0,rbound)
@@ -49,18 +62,26 @@ mi_addsegment(0,-rbound,0,rbound)
 mi_addarc(0,-rbound,0,rbound,180,1)
 
 --Materialien laden und zuweisen
-mi_addcircprop('circuit', Icoil, 1) --Stromkreis: I A, in Serie
+mi_addcircprop('circuit1', Icoil1, 1) --Stromkreis: I A, in Serie
+mi_addcircprop('circuit2', Icoil2, 1) --Stromkreis: I A, in Serie
 
 --Materialien einlesen
 mi_getmaterial('Air')	
 mi_getmaterial('Copper')
 mi_getmaterial('Steel castings, as cast');
 
--- Spule
-mi_addblocklabel(rx+d/2,0)	
-mi_selectlabel(rx+d/2,0)	
-mi_setblockprop('Copper',am,ms,'circuit',90,0,Ncoil)    
+-- Spule 1
+mi_addblocklabel(rx+d/2,(-h-h_sh)/2)	
+mi_selectlabel(rx+d/2,(-h-h_sh)/2)	
+mi_setblockprop('Copper',am,ms,'circuit1',90,0,Ncoil1)    
 mi_clearselected()
+
+-- Spule 2
+mi_addblocklabel(rx+d/2,(h+h_sh)/2)	
+mi_selectlabel(rx+d/2,(h+h_sh)/2)	
+mi_setblockprop('Copper',am,ms,'circuit2',90,0,Ncoil2)    
+mi_clearselected()
+
 
 --Kugel
 mi_addblocklabel(1,ky)	
@@ -86,9 +107,10 @@ mi_zoomnatural()
 --mi_createmesh() --works, but pops-up window which has to be closed manually
 
 
-for Icoil=0,10,2 do 
+--for Icoil=0,10,2 do 
 	--Strom nochmal laden / aendern
-	mi_addcircprop('circuit', Icoil, 1) --Stromkreis: I A, in Serie
+	mi_addcircprop('circuit1', Icoil1, 1) --Stromkreis: I A, in Serie
+	mi_addcircprop('circuit2', Icoil2, 1) --Stromkreis: I A, in Serie
 
 	mi_analyze(0)
 	mi_loadsolution()
@@ -107,7 +129,7 @@ for Icoil=0,10,2 do
 	Eco = mo_blockintegral(17)
 	mo_clearblock()
 
-	str = Icoil .. ", ".. ky .. ", " .. Fx .. ", " .. Fy .. ", " .. Eco
+	str = Icoil1 .. ", ".. ky .. ", " .. Fx .. ", " .. Fy .. ", " .. Eco
 	--print(str)
 	write(file,str.."\n")
 
@@ -117,7 +139,7 @@ for Icoil=0,10,2 do
 
 	mo_savebitmap("outfile".. istep .. ".bmp")
 	istep = istep + 1;
-end
+--end
 
 
 mi_close()  --close preprocessor
--- fem/aircoil_FEMM/eval_result.m
+++ fem/aircoil_FEMM/eval_result.m
@@ -1,9 +1,51 @@
-load('output.csv')
-I = output(:,1);
-ky = output(:,2);
-Fy = output(:,4);
-Eco = output(:,5);
+M = csvread('output.csv');
+y = M(:,2);
+F = M(:,4);
 
-scatter3(I,ky, Fy)
+%Modell (Kloss)
+y0 = -6;
+yk = 5;
+Fk = 1.51;
+Fmod = -Fk * 2./( (y-y0)./yk  +  yk./(y-y0)  );
 
-scatter3(I,ky, Eco)
+plot(y,F,'b-','LineWidth',2)
+hold on;
+plot(y+12,F,'r-','LineWidth',2)
+%plot(-y,-F,'g-','LineWidth',2)
+plot(y,Fmod,'g--','LineWidth',1)
+hold off
+
+
+
+clear myDim;
+%Abmaße
+myDim.x_cm = 16;
+myDim.y_cm = 8.5;
+
+%Ursprung KOS
+myDim.x_cm_orig = 11; 
+myDim.y_cm_orig = 3.5;
+
+%Minimum KOS
+myDim.x_cm_min = 0.375;
+myDim.y_cm_min = 0.375;
+
+%Maximum KOS
+myDim.x_cm_max = 14;
+myDim.y_cm_max = 7.0;
+
+myDim.y_scale = 0.5;
+myDim.x_scale = 2.0;
+
+myDim.y_tickres = 0.5;
+myDim.x_tickres = 2.0;
+
+%myDim.x_noscale = 1;
+%myDim.y_noscale = 1;
+
+myDim.x_label = '{\it{y}} / mm';
+myDim.y_label = '{\it{F}}_y / N';
+
+
+ah = gca();
+%drawPaper(myDim,'y_Fy',ah);
