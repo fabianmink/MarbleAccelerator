be9b5a7f73c690c1fedd7f3f6d2e10d97998c784
Fabian Mink THM <fabian.mink@iem.thm.de>
2023-01-13 10:37:49 +0100

    grid data

--- fem/aircoil_FEMM/eval_result_2d.m
+++ fem/aircoil_FEMM/eval_result_2d.m
@@ -1,4 +1,4 @@
-M = csvread('output.csv');
+M = csvread('output_2d_fein.csv');
 I = M(:,1);
 y = M(:,2);
 F = M(:,4);
@@ -7,11 +7,27 @@ Emag = M(:,6);
 
 %F_from_Eco = [0; diff(Eco)./ (diff(y)*0.001)];  %y is in mm
 
-scatter3(I,y,F);
+%Extend to positive y
+Iext = [I;I];
+yext = [y;-y];
+Fext = [F;-F];
+
+%scatter3(I,y,F);
+
 zlabel('F/N')
 xlabel('I/A')
 ylabel('y/mm')
 
+
+Igrid = 0:1:30;
+ygrid = -20:0.25:20;
+
+Fgrid = griddata(Iext,yext,Fext,Igrid,ygrid');
+
+%scatter3(Iext,yext,Fext);
+%hold on;
+surf(Igrid,ygrid,Fgrid);
+%hold off;
 
 %test
 % subplot(2,1,1)
