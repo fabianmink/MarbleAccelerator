8672fb374571adf9e713be9adfee013aff409baa
Fabian Mink THM <fabian.mink@iem.thm.de>
2025-03-20 16:48:18 +0100

    simulation added

---
+++ sim/parameters.m
@@ -0,0 +1,7 @@
+Lmin =  1.29e-3;  %Inductance (for marble out of coil)
+d =       0.396;  %Relative increase of inductance (for marble in middle of coil)
+zs =    9.60e-3;  %m 
+z0 =  -10.51e-3;  %m
+
+R = 2;
+m = 8.5e-3;
