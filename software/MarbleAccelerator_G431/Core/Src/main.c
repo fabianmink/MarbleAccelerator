d2a96fe24711e00cdba8f10457a6be5c073bb4a6
Fabian Mink THM <fabian.mink@iem.thm.de>
2025-03-24 12:19:29 +0100

    project rename part 2

--- software/MarbleAccelerator_G431/Core/Src/main.c
+++ software/MarbleAccelerator_G431/Core/Src/main.c
@@ -300,7 +300,7 @@ void SystemClock_Config(void)
   {
   }
 
-  /* Insure 1µs transition state at intermediate medium speed clock based on DWT */
+  /* Insure 1Âµs transition state at intermediate medium speed clock based on DWT */
   CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
   DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
   DWT->CYCCNT = 0;
