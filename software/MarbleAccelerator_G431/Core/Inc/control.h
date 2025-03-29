d582bca788604df9183a99ab2bb31d8d54c1aa88
Fabian Mink THM <fabian.mink@iem.thm.de>
2025-03-29 06:46:31 +0100

    remove of unnecessary defines

--- software/MarbleAccelerator_G431/Core/Inc/control.h
+++ software/MarbleAccelerator_G431/Core/Inc/control.h
@@ -1,5 +1,8 @@
 #include "main.h"
 
+//#define SINGLECOIL_DESIGN
+
+
 extern FDCAN_HandleTypeDef hfdcan1;
 
 #include <stdint.h>
@@ -22,19 +25,5 @@ typedef struct
 } calib_data_t;
 
 
-
-#define WFB_SYNC_TIMER_PERIOD    50000   //10ms
-#define WFB_SYNC_REF             8600    //@ 1.72ms = 50000 * 0.172
-
-#define WFB_SLAVE_TX_POS_OFFS    10000   //@ 2.0ms = 50000 * 0.20, Offset for slave 0
-#define WFB_SLAVE_TX_POS_SHIFT   10000   //@ 2.0ms, every slave is shifted by this value
-
-#define WFB_MASTER_DATA_BYTES_BASIC       2  // 1 general command + 1 service
-#define WFB_MASTER_DATA_BYTES_PER_SLAVE   5  // 1 individual command + 4 position
-#define WFB_MASTER_DATA_BYTES_HMAC        4
-
-#define INSYNC_CNT_DIFF         250     //50us = 50000 * 0.005
-#define INSYNC_CNT_LEN          20
-#define INSYNC_MAX_LOST_FRAMES  3
 
 
