7afad84cb7a42c6ad4776992481280b5cdaaaa69
Fabian Mink THM <fabian.mink@iem.thm.de>
2025-04-05 14:15:46 +0200

    model with capacitor / high-side switch added

--- sim/parameters.m
+++ sim/parameters.m
@@ -5,3 +5,11 @@ zs =    7.14e-3;  %m
 
 R = 0.7;
 m = 8.5e-3;
+
+
+%For model with Capacitor
+C = 40e-3;
+Tpulse = 18e-3;
+uc0 = 24;
+uclamp = 67; %Clamping voltag of integrated High-Side switch
+Rcharge = 20;
